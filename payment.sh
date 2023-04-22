3b224db4446cef1b00309686a9fe4f806d9b405b
Raju <itrajuaws@gmail.com>
2023-04-22 06:19:20 +0530

    Source Common

--- payment.sh
+++ payment.sh
@@ -1,27 +1,12 @@
-echo -e "\e[36m>>>>>>>>> Install Python <<<<<<<<\e[0m"
-yum install python36 gcc python3-devel -y
+script=$(realpath "$0")
+script_path=$(dirname "$script")
+source ${script_path}/common.sh
+rabbitmq_appuser_password=$1
 
-echo -e "\e[36m>>>>>>>>> Add Application User <<<<<<<<\e[0m"
-useradd roboshop
+if [ -z "$rabbitmq_appuser_password" ]; then
+  echo Input Roboshop Appuser Password Missing
+  exit
+fi
 
-echo -e "\e[36m>>>>>>>>> Create App Dir <<<<<<<<\e[0m"
-rm -rf /app
-mkdir /app
-
-echo -e "\e[36m>>>>>>>>> Download App Content <<<<<<<<\e[0m"
-curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
-
-echo -e "\e[36m>>>>>>>>> Extract App Content <<<<<<<<\e[0m"
-cd /app
-unzip /tmp/payment.zip
-
-echo -e "\e[36m>>>>>>>>> Install Dependencies <<<<<<<<\e[0m"
-pip3.6 install -r requirements.txt
-
-echo -e "\e[36m>>>>>>>>>Setup SystemD Service <<<<<<<<\e[0m"
-cp /home/centos/roboshop-shell/payment.service /etc/systemd/system/payment.service
-
-echo -e "\e[36m>>>>>>>>> Start Payment Service <<<<<<<<\e[0m"
-systemctl daemon-reload
-systemctl enable payment
-systemctl restart payment
+component=payment
+func_python
